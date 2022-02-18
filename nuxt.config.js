57be00a887826d8c07d45c6dca996b866bfd0f14
Teerut26 <teerutsr@gmail.com>
2022-02-18 14:05:06 +0700

    init commit.

---
+++ nuxt.config.js
@@ -0,0 +1,79 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt-tailwind-vuex',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/assets/css/main.css',
+  ],
+   serverMiddleware: [
+    '~/api/auth',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    '@nuxt/postcss8',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/auth-next'
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    proxy: true
+  },
+  auth: {
+  strategies: {
+    local: {
+        token: {
+          property: 'token.accessToken'
+        }
+      },
+      localRefresh: {
+        scheme: 'refresh',
+        token: {
+          property: 'token.accessToken',
+          maxAge: 15
+        },
+        refreshToken: {
+          property: 'token.refreshToken',
+          data: 'refreshToken',
+          maxAge: false
+        }
+      },
+  }
+},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    class="flex gap-3 absolute top-0 bottom-0 right-0 left-0 flex-col justify-center items-center bg-gradient-to-r from-sky-500 to-red-400"
+  >
+    <div>
+      <img class="drop-shadow-xl" src="/favicon.ico" alt="nuxt logo" />
+    </div>
+    <div v-if="this.$auth.user" class="flex items-center gap-3 bg-white rounded-xl px-3 py-2">
+        <div>
+            <img class="w-12 h-12 rounded-full" :src="this.$auth.user.picture" alt="">
+        </div>
+        <div class="flex flex-col">
+            <div>{{this.$auth.user.username}}</div>
+            <div>exp {{new Date(this.$auth.user.exp*1000).toLocaleString("th-TH")}}</div>
+        </div>
+        <div @click="$auth.logout()" class="bg-red-500 hover:bg-red-600 px-3 py-1 text-white rounded-xl cursor-pointer select-none">
+            Sign Out
+        </div>
+    </div>
+    <div class="text-3xl text-white">
+      Count : {{ this.$store.state.counter }}
+    </div>
+    <div class="flex gap-2">
+      <div
+        @click="$store.commit('increase')"
+        class="py-2 px-3 bg-sky-500 hover:bg-sky-600 rounded-xl text-white cursor-pointer select-none"
+      >
+        Increase
+      </div>
+      <div
+        @click="$store.commit('decrease')"
+        class="py-2 px-3 bg-sky-500 hover:bg-sky-600 rounded-xl text-white cursor-pointer select-none"
+      >
+        Decrease
+      </div>
+      <NuxtLink
+        to="/login"
+        class="py-2 px-3 bg-sky-500 hover:bg-sky-600 rounded-xl text-white cursor-pointer select-none"
+      >
+        Login
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+    // methods: {
+    //   increase() {
+    //     this.$store.commit('increase')
+    //   },
+    // },
+}
+</script>
